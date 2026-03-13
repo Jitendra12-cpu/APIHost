ea6918b073b5a51fe002aefd16f9498075220310
jitendra <jitendrapandey5653@gmail.com>
2026-03-13 18:26:25 +1300

    jitendra: added a files of hosting

---
+++ src/main/java/cs/jitendra/hostingAPI/HostingApiApplication.java
@@ -0,0 +1,13 @@
+package cs.jitendra.hostingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostingApiApplication.class, args);
+	}
+
+}
